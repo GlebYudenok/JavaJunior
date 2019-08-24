adcbf3803a99adf471089ceadcfa92d29f5da17a
user <gyudenok@mail.ru>
2019-08-24 18:31:46 +0300

    add delete by id method realization

--- src/main/java/by/gyudenok/controller/impl/UserDeleteByIdCommand.java
+++ src/main/java/by/gyudenok/controller/impl/UserDeleteByIdCommand.java
@@ -1,11 +1,28 @@
 package by.gyudenok.controller.impl;
 
 import by.gyudenok.controller.Command;
+import by.gyudenok.controller.scanner.DataEntry;
+import by.gyudenok.controller.scanner.DataFormatter;
+import by.gyudenok.controller.scanner.impl.UserDataFormatter;
+import by.gyudenok.dao.DAO;
+import by.gyudenok.dao.factory.DAOFactory;
+import by.gyudenok.domain.User;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.IOException;
 
 public class UserDeleteByIdCommand implements Command {
 
+    private final DAO<User> userDAO = DAOFactory.getInstance()
+            .getFileUserDAO();
+    private final static Logger LOGGER = LogManager.getLogger(UserReadCommand.class);
+
     @Override
-    public String executeTask(String request) {
-        return null;
+    public String executeTask(String request) throws IOException {
+        LOGGER.info("Enter index of user which need delete: ");
+        int id = DataEntry.enterInt();//validation
+        userDAO.deleteById(id);
+        return "User with " + id + " id was deleted successfully!";
     }
 }
