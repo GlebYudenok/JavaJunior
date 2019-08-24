7d91e7545b1379a464486336baa0da1904706df2
user <gyudenok@mail.ru>
2019-08-24 18:32:21 +0300

    add delete by index method realization

--- src/main/java/by/gyudenok/controller/impl/UserDeleteByIndexCommand.java
+++ src/main/java/by/gyudenok/controller/impl/UserDeleteByIndexCommand.java
@@ -1,11 +1,26 @@
 package by.gyudenok.controller.impl;
 
 import by.gyudenok.controller.Command;
+import by.gyudenok.controller.scanner.DataEntry;
+import by.gyudenok.dao.DAO;
+import by.gyudenok.dao.factory.DAOFactory;
+import by.gyudenok.domain.User;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.IOException;
 
 public class UserDeleteByIndexCommand implements Command {
 
+    private final DAO<User> userDAO = DAOFactory.getInstance()
+            .getFileUserDAO();
+    private final static Logger LOGGER = LogManager.getLogger(UserReadCommand.class);
+
     @Override
-    public String executeTask(String request) {
-        return null;
+    public String executeTask(String request) throws IOException {
+        LOGGER.info("Enter index of user which need delete: ");
+        int index = DataEntry.enterInt();//validation
+        userDAO.deleteByIndex(index);
+        return "User with " + index + " index was deleted successfully!";
     }
 }
