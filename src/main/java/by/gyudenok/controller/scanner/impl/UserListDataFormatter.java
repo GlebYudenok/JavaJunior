e33dc1e33b9cbba4ce6344d01f3babe546d00e1b
user <gyudenok@mail.ru>
2019-08-24 18:34:22 +0300

    add user list data formatter class

---
+++ src/main/java/by/gyudenok/controller/scanner/impl/UserListDataFormatter.java
@@ -0,0 +1,19 @@
+package by.gyudenok.controller.scanner.impl;
+
+import by.gyudenok.controller.scanner.DataFormatter;
+import by.gyudenok.domain.User;
+
+import java.util.List;
+
+public class UserListDataFormatter implements DataFormatter<List<User>> {
+
+    @Override
+    public String formatUser(List<User> userList) {
+        DataFormatter<User> userFormatter = new UserDataFormatter();
+        String response = new String("Users: \n");
+        for(User user : userList){
+            response += userFormatter.formatUser(user);
+        }
+        return response;
+    }
+}
