30745bd69703012a3809a3d6f89345025f74eaf5
user <gyudenok@mail.ru>
2019-08-24 18:33:32 +0300

    add read user by index method realization

--- src/main/java/by/gyudenok/controller/impl/UserReadCommand.java
+++ src/main/java/by/gyudenok/controller/impl/UserReadCommand.java
@@ -1,11 +1,30 @@
 package by.gyudenok.controller.impl;
 
 import by.gyudenok.controller.Command;
+import by.gyudenok.controller.scanner.DataEntry;
+import by.gyudenok.controller.scanner.DataFormatter;
+import by.gyudenok.controller.scanner.impl.UserDataFormatter;
+import by.gyudenok.dao.DAO;
+import by.gyudenok.dao.factory.DAOFactory;
+import by.gyudenok.domain.User;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.xml.crypto.Data;
+import java.io.IOException;
 
 public class UserReadCommand implements Command {
 
+    private final DAO<User> userDAO = DAOFactory.getInstance()
+            .getFileUserDAO();
+    private final static Logger LOGGER = LogManager.getLogger(UserReadCommand.class);
+
     @Override
-    public String executeTask(String request) {
-        return null;
+    public String executeTask(String request) throws IOException {
+        LOGGER.info("Enter index of user: ");
+        int id = DataEntry.enterInt();//validation
+        User u = userDAO.read(id);
+        DataFormatter<User> formatter = new UserDataFormatter();
+        return formatter.formatUser(u);
     }
 }
